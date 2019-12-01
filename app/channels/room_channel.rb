15539fbef372b2cf370d32d3ae522233b7db42da
github user name <ryunosuke_yamamoto@yahoo.co.jp>
2019-12-01 15:15:42 +0900

    第二の下準備まで完了

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -9,5 +9,6 @@ class RoomChannel < ApplicationCable::Channel
 
   def speak(data)
     Message.create!(content: data['message'])
+    ActionCable.server.broadcast 'room_channel',data['message']
   end
 end
